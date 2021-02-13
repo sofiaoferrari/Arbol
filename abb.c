d7374456d85b09d5b13bf7cd5d0f1ee916443254
sofiaoferrari <soferrari@fi.uba.ar>
2021-02-12 21:01:46 -0300

    aaaaa

--- abb.c
+++ abb.c
@@ -9,40 +9,37 @@
 #define ELIMINAR -1
 
 #define IGUAL 0
-#define MENOR 1
-#define MAYOR -1
+#define MENOR 1     //izquierda
+#define MAYOR -1   //derecha
 
 #define SIN_HIJOS 0
-#define UN_HIJO 1
+#define UN_HIJO_I 1
+#define UN_HIJO_D -1
 #define DOS_HIJOS 2
 
-typedef struct nodos {
-    nodo_abb_t* hijo_izquierdo;
-    nodo_abb_t* hijo_derecho;
-    nodo_abb_t* nuevo_padre;
-} nodos_t;
-
 bool arbol_vacio(abb_t* arbol) {
-    if (!arbol) return true;
-    else if (!(arbol->nodo_raiz)) return true;
+   
+    if (!arbol || !(arbol->nodo_raiz)) return true;
+  
     return false;
 }
 
 void* arbol_raiz(abb_t* arbol) {
-    if (!arbol || !(arbol->nodo_raiz)) return NULL;
+    if (!arbol || !(arbol->nodo_raiz))
+         return NULL;
     return arbol->nodo_raiz->elemento;
 }
 
 /*
  * Procedimiento recursivo 
  */
-void destruir_nodo(abb_t* arbol, nodo_abb_t* nodo_hijo) { //recorrido postorden
+void arbol_eliminar_elementos_recursivamente(abb_t* arbol, nodo_abb_t* nodo_hijo) { //recorrido postorden
     if (!nodo_hijo) return;
     if (nodo_hijo->izquierda) {
-        destruir_nodo(arbol, nodo_hijo->izquierda);
+        arbol_eliminar_elementos_recursivamente(arbol, nodo_hijo->izquierda);
     }
     if (nodo_hijo->derecha) {
-        destruir_nodo(arbol, nodo_hijo->derecha);
+        arbol_eliminar_elementos_recursivamente(arbol, nodo_hijo->derecha);
         
     }
     
@@ -54,12 +51,10 @@ void destruir_nodo(abb_t* arbol, nodo_abb_t* nodo_hijo) { //recorrido postorden
 
 void arbol_destruir(abb_t* arbol) {
     if (arbol && (!arbol_vacio(arbol))) {
-        destruir_nodo(arbol, arbol->nodo_raiz);
-
+        arbol_eliminar_elementos_recursivamente(arbol, arbol->nodo_raiz);
     }
-    if (arbol) {
-        free(arbol);
-    }
+    
+    free(arbol);
 }
 
 /*
@@ -67,57 +62,75 @@ void arbol_destruir(abb_t* arbol) {
  * 
 */
 nodo_abb_t* encontrar_elemento(abb_comparador comparador, nodo_abb_t* nodo_raiz, void* elemento, int accion, nodo_abb_t* nodo_anterior) {
+    if (!nodo_raiz) return NULL;
     int comparacion = comparador(nodo_raiz->elemento, elemento);
     //nodo_anterior = nodo_raiz;
     if ((comparacion == IGUAL) && ((accion == BUSCAR) || (accion == ELIMINAR))) {
-        //printf("\nELEMENTO ES IGUAL\n");
-        nodo_raiz = nodo_raiz;
+        printf("\nELEMENTO ES IGUAL\n");
+        nodo_raiz = nodo_raiz;      
     }
-    else if (((comparacion == MAYOR) || (comparacion == IGUAL)) && (nodo_raiz->derecha)) {
-        //printf("\nENTRO A MAYOR\n");
+    else if (((comparacion == MAYOR)) && (nodo_raiz->derecha)) {
+        printf("\nENTRO A MAYOR\n");
         nodo_anterior = nodo_raiz;
         nodo_raiz = encontrar_elemento(comparador,nodo_raiz->derecha, elemento, accion, nodo_anterior);
     }
     else if ((comparacion == MENOR) && (nodo_raiz->izquierda)) {
-        //printf("\nENTRO A MENOR\n");
+        printf("\nENTRO A MENOR\n");
         nodo_anterior = nodo_raiz;
         nodo_raiz = encontrar_elemento(comparador,nodo_raiz->izquierda, elemento, accion, nodo_anterior);
     }
     
-    if (accion == ELIMINAR) {
-        //printf("\nELEMENTO NODO ANTERIOR: %d\n", *(int*)(nodo_anterior->elemento));
+    if (accion == ELIMINAR && comparacion == IGUAL) {
+        printf("\nELEMENTO NODO ANTERIOR: %d\n", *(int*)(nodo_anterior->elemento));
         return nodo_anterior;
     }
-
+     
     return nodo_raiz;
 }
 
 void* arbol_buscar(abb_t* arbol, void* elemento) {
     if (!arbol || !elemento || arbol_vacio(arbol) || !arbol->comparador) return NULL;
     nodo_abb_t* encontrado = NULL;
+    printf("\nELEMENTO DE RAIZ: %d\n", *(int*)arbol->nodo_raiz->elemento); 
+  //  printf("\nELEMENTO DE RAIZ: %d\n", *(int*)arbol->nodo_raiz->izquierda->elemento);   
+   // printf("\nELEMENTO DE RAIZ: %d\n", *(int*)arbol->nodo_raiz->derecha->elemento);   
+
+    printf("\nELEMENTO A BUSCAR: %d\n", *(int*)elemento);
     encontrado = encontrar_elemento(arbol->comparador, arbol->nodo_raiz, elemento, BUSCAR, encontrado);
-    
+    printf("\nELEMENTO ENCONTRADO: %d\n", *(int*)(encontrado->elemento));
+    int comparacion = arbol->comparador(elemento, encontrado->elemento);
+    if (comparacion != IGUAL) return NULL;
+  /*  
     if (encontrado) {
         printf("\nENTRO A encontrado\n");
-        int comparacion = arbol->comparador(elemento, encontrado->elemento);
-        if (comparacion != IGUAL) return NULL;
         //printf("\nCOMPARACION: %d\n", comparacion);
-        //printf("\nELEMENTO ENCONTRADO: %d\n", *(int*)(encontrado->elemento));
-        //printf("\nELEMENTO A BUSCAR: %d\n", *(int*)elemento);
-        //printf("\nELEMENTO DEL HIJO DE RAIZ: %d\n", *(int*)arbol->nodo_raiz->derecha->elemento);   
-    }
+    }*/
         
     return encontrado->elemento;
 }
 
-nodo_abb_t* elemento_mas_proximo(nodo_abb_t* nodo_hijo, nodo_abb_t* nodo_anterior) { 
-    if (nodo_hijo->derecha) {
-        nodo_anterior = nodo_hijo;
-        nodo_hijo = nodo_hijo->derecha;
-        nodo_hijo = elemento_mas_proximo(nodo_hijo, nodo_anterior);
+/*
+ * Funcion recursiva
+*/
+nodo_abb_t* elemento_sucesor_inorden(nodo_abb_t* nodo_hijo, nodo_abb_t* nodo_padre) { //lamara con derecha
+    if (nodo_hijo->izquierda) {
+        nodo_padre = nodo_hijo;
+        nodo_hijo = nodo_hijo->izquierda;
+        nodo_hijo = elemento_sucesor_inorden(nodo_hijo, nodo_padre);
     }
     return nodo_hijo;
 }
+
+
+nodo_abb_t* buscar_hijo_mayor(nodo_abb_t* nodo_hijo, nodo_abb_t* nodo_padre) { 
+    if (nodo_hijo->derecha) {
+        nodo_padre = nodo_hijo;
+        nodo_hijo = nodo_hijo->derecha;
+        nodo_hijo = buscar_hijo_mayor(nodo_hijo, nodo_padre);
+    }
+    return nodo_hijo;
+}
+
 /*
 nodo_abb_t* elemento_menor(nodo_abb_t* hijo_menor) {
     if (hijo_menor->izquierda) {
@@ -127,11 +140,15 @@ nodo_abb_t* elemento_menor(nodo_abb_t* hijo_menor) {
     return hijo_menor;
 }*/
 
+/*
+ * Procedimiento 
+*/
+/*
 void nodo_con_dos_hijos(abb_t* arbol, nodo_abb_t* nodo_a_borrar, nodos_t* nuevo_arbol, int es_la_raiz) {
     nuevo_arbol->hijo_izquierdo = nodo_a_borrar->izquierda;
     nuevo_arbol->hijo_derecho = nodo_a_borrar->derecha;
     nodo_abb_t* nodo_anterior = arbol->nodo_raiz;
-    nuevo_arbol->nuevo_padre = elemento_mas_proximo(nodo_a_borrar->izquierda, nodo_anterior);
+    nuevo_arbol->nuevo_padre = elemento_mas_aproximado_al_elemento_a_borrar(nodo_a_borrar->izquierda, nodo_anterior);
     int comparacion = arbol->comparador(nodo_anterior->elemento, nuevo_arbol->nuevo_padre->elemento);
 
     if (nuevo_arbol->nuevo_padre->izquierda) {
@@ -149,6 +166,7 @@ void nodo_con_dos_hijos(abb_t* arbol, nodo_abb_t* nodo_a_borrar, nodos_t* nuevo_
         nodo_anterior->izquierda = NULL;
 }
 
+
 void nodo_con_hijo(nodo_abb_t* nodo_a_borrar, nodos_t* nuevo_arbol) {  //no necesito comparacion
 
     if (nodo_a_borrar->derecha)
@@ -156,6 +174,7 @@ void nodo_con_hijo(nodo_abb_t* nodo_a_borrar, nodos_t* nuevo_arbol) {  //no nece
     if (nodo_a_borrar->izquierda)
         nuevo_arbol->nuevo_padre = nodo_a_borrar->izquierda;
 }
+
 
 int borrar(abb_t* arbol, nodo_abb_t* nodo_anterior, nodo_abb_t* nodo_a_borrar, int hijos, int es_la_raiz){
     nodos_t* nuevo_arbol = calloc(1, sizeof(nodo_abb_t));
@@ -185,7 +204,7 @@ int borrar(abb_t* arbol, nodo_abb_t* nodo_anterior, nodo_abb_t* nodo_a_borrar, i
     } else if ((comparacion == MENOR ) && (!es_la_raiz)) {
         printf("\nentro aca\n");
 
-         if (hijos == SIN_HIJOS) 
+        if (hijos == SIN_HIJOS) 
             nodo_anterior->izquierda = NULL;
         else 
             nodo_anterior->izquierda = nuevo_arbol->nuevo_padre;      
@@ -196,31 +215,113 @@ int borrar(abb_t* arbol, nodo_abb_t* nodo_anterior, nodo_abb_t* nodo_a_borrar, i
     //free(nuevo_arbol->nuevo_padre);
     free(nuevo_arbol);
     return EXITO;
+}*/
+
+nodo_abb_t* nodo_con_dos_hijos(abb_t* arbol, nodo_abb_t* nodo_padre, nodo_abb_t* nodo_a_borrar, nodo_abb_t* sucesor) {
+    nodo_abb_t* hijo_mayor = NULL;
+    nodo_abb_t* padre_del_sucesor = nodo_padre;
+    sucesor = elemento_sucesor_inorden(nodo_a_borrar->derecha, padre_del_sucesor);
+    bool hijo_es_sucesor = !(arbol->comparador(nodo_a_borrar->derecha->elemento, sucesor->elemento));
+        printf("\nNodo sucesor elemento:%d", *(int*)sucesor->elemento);
+        
+        printf("\nNodo a borrar elemento:%d", *(int*)nodo_a_borrar->elemento);
+        printf("\nNODO hijo derecho:%d", *(int*)nodo_a_borrar->derecha->elemento);
+
+        
+        printf("\nHIJO ES SUCESOR:%d", hijo_es_sucesor);
+
+    if (!hijo_es_sucesor) {
+        printf("\nNODO PADRE DEL SUCESOR:%d", *(int*)padre_del_sucesor->elemento);
+        padre_del_sucesor = encontrar_elemento(arbol->comparador, arbol->nodo_raiz, sucesor->elemento, ELIMINAR, arbol->nodo_raiz);
+        printf("\nNODO PADRE DEL SUCESOR:%d", *(int*)padre_del_sucesor->elemento);
+        padre_del_sucesor->izquierda = NULL;
+        if(!sucesor->derecha)
+            sucesor->derecha = nodo_a_borrar->derecha;
+        else if (sucesor->derecha) {
+            hijo_mayor = buscar_hijo_mayor(sucesor->derecha, padre_del_sucesor);
+            printf("\nHIJO MAYORRRRR:%d", *(int*)hijo_mayor->elemento);
+            hijo_mayor->derecha = nodo_a_borrar->derecha;
+        }
+    }
+    sucesor->izquierda = nodo_a_borrar->izquierda;
+    return sucesor;
+}
+
+
+void borrar_segun_hijos(abb_t* arbol, nodo_abb_t* nodo_padre, nodo_abb_t* nodo_a_borrar, int hijos, bool es_la_raiz, int comparacion){
+    nodo_abb_t* sucesor_a_nodo_borrar = NULL; // nuevo nodo hijo
+    if (hijos == UN_HIJO_D)
+        sucesor_a_nodo_borrar = nodo_a_borrar->derecha;
+    else if (hijos == UN_HIJO_I)
+        sucesor_a_nodo_borrar = nodo_a_borrar->izquierda;
+    else if (hijos == DOS_HIJOS) {
+       sucesor_a_nodo_borrar = nodo_con_dos_hijos(arbol, nodo_padre, nodo_a_borrar, sucesor_a_nodo_borrar);
+    }
+        printf("\ncompariacon %d \n", comparacion);  
+    
+    if (comparacion == MAYOR)  // el nodo a borrar es mayor que su padre
+        nodo_padre->derecha = sucesor_a_nodo_borrar;
+    else if (comparacion == MENOR) // el nodo a borrar es menor que su padre
+        nodo_padre->izquierda = sucesor_a_nodo_borrar;
+    if (es_la_raiz) {
+        printf("\nENtro a Es raiz \n");  
+        arbol->nodo_raiz = sucesor_a_nodo_borrar;
+
+    //printf("\nElemento sucesor: %d\n", *(int*)sucesor_a_nodo_borrar->elemento);
+   // printf("\nElemento sucesor iz: %d\n", *(int*)sucesor_a_nodo_borrar->izquierda->elemento);
+   // printf("\nElemento sucesor der: %d\n", *(int*)sucesor_a_nodo_borrar->derecha->elemento);
+    }
+    
+
+
+    arbol->destructor(nodo_a_borrar->elemento);
+    free(nodo_a_borrar);
+}
+
+int nodo_tiene_hijos(nodo_abb_t* nodo_a_borrar) {
+    int hijos = 0;
+    if ((!nodo_a_borrar->derecha) && (!nodo_a_borrar->izquierda)) 
+        hijos = SIN_HIJOS;
+    else if (!nodo_a_borrar->derecha)  
+        hijos = UN_HIJO_I;
+    else if (!nodo_a_borrar->izquierda){
+        hijos = UN_HIJO_D;
+        printf("\nhijo derecho que no existeeeeeeeeee: %d \n", *(int*)nodo_a_borrar->derecha);  
+
+    }
+    else if (nodo_a_borrar->derecha && nodo_a_borrar->izquierda) 
+        hijos = DOS_HIJOS;
+    return hijos;
 }
 
 int arbol_borrar(abb_t* arbol, void* elemento) {
     if (!arbol || !elemento || arbol_vacio(arbol)) return ERROR;
-    else if (!arbol->destructor || !arbol->comparador) return ERROR;
-    nodo_abb_t* nodo_anterior = arbol->nodo_raiz;
+    else if (!arbol->destructor) return ERROR;
     nodo_abb_t* nodo_a_borrar = NULL;
-    nodo_anterior = encontrar_elemento(arbol->comparador, nodo_anterior, elemento, ELIMINAR, nodo_anterior);
-    nodo_a_borrar = encontrar_elemento(arbol->comparador, nodo_anterior, elemento, BUSCAR, nodo_anterior);
+    nodo_abb_t* nodo_padre = arbol->nodo_raiz;
+    nodo_a_borrar = encontrar_elemento(arbol->comparador, nodo_padre, elemento, BUSCAR, nodo_padre);
+    printf("\nnodo a BOttarrr: %d\n", *(int*)nodo_a_borrar->elemento);
+    printf("\nRAIZ: %d\n", *(int*)arbol->nodo_raiz->elemento);
     
-    int es_la_raiz = !(arbol->comparador(arbol->nodo_raiz->elemento, nodo_a_borrar->elemento));
-    int tiene_hijos = SIN_HIJOS;
-    int se_pudo = EXITO;
-    if ((!nodo_a_borrar->derecha) && (!nodo_a_borrar->izquierda)) 
-        tiene_hijos = SIN_HIJOS;
-    else if ((!nodo_a_borrar->derecha) || (!nodo_a_borrar->izquierda)) 
-        tiene_hijos = UN_HIJO;
-    else if (nodo_a_borrar->derecha && nodo_a_borrar->izquierda) 
-        tiene_hijos = DOS_HIJOS;
-    printf("\ncantidad de hijos: %d\n", tiene_hijos);
+    if (!nodo_a_borrar) return ERROR;  //el elemento no se encuentra en el arbol
+    int hijos = nodo_tiene_hijos(nodo_a_borrar);
+    printf("\ncantidad de hijos: %d\n", hijos);
+    nodo_padre = encontrar_elemento(arbol->comparador, nodo_padre, elemento, ELIMINAR, nodo_padre);
+    printf("\nnodo padre del borrar: %d\n", *(int*)nodo_padre->elemento);
+    
+    bool es_la_raiz = !(arbol->comparador(arbol->nodo_raiz->elemento, elemento));
+    int comparacion = arbol->comparador(nodo_padre->elemento, elemento);
+
+    printf("\n%d ES LA RAIZ: %d\n", *(int*)elemento,es_la_raiz);
+    borrar_segun_hijos(arbol, nodo_padre, nodo_a_borrar, hijos, es_la_raiz, comparacion);
+    //printf("\ncantidad de hijos: %d\n", nodo_tiene_hijos(arbol->nodo_raiz));
+    
     //printf(" \nhijo derecho: %d\n", *(int*)nodo_a_borrar->izquierda->elemento);
-    
-    se_pudo = borrar(arbol, nodo_anterior, nodo_a_borrar, tiene_hijos, es_la_raiz);
-    
-    return se_pudo;
+    //printf("\nELEMENTO DE RAIZ: %d\n", *(int*)arbol->nodo_raiz->elemento); 
+    //printf("\nELEMENTO izq DE RAIZ: %d\n", *(int*)arbol->nodo_raiz->izquierda->elemento);   
+    //printf("\nELEMENTO der DE RAIZ: %d\n", *(int*)arbol->nodo_raiz->derecha->elemento); 
+
+    return EXITO;
 }
 
 /*
@@ -246,19 +347,26 @@ int arbol_insertar(abb_t* arbol, void* elemento) {
     else {
         nodo_abb_t* nodo = NULL;
         nodo = encontrar_elemento(arbol->comparador, arbol->nodo_raiz, elemento, INSERTAR, nodo);
+        printf("\nELEMENTO ultimo nodo(?): %d\n", *(int*)nodo->elemento);  
         if (nodo) { //no tiene hijos
             int comparacion = arbol->comparador(nodo->elemento, elemento);
-            if (comparacion == MAYOR || comparacion == IGUAL)
+            printf("\ncomparacion: %d\n", comparacion);  
+            if (comparacion == MAYOR || comparacion == IGUAL){
                 insertado = insertar_elemento(&nodo->derecha, elemento);
-            else if (comparacion == MENOR)  
+                printf("\nELEMENTO der DE RAIZ: %d\n", *(int*)arbol->nodo_raiz->derecha->elemento);  
+
+            }
+            else if (comparacion == MENOR)  {
                 insertado = insertar_elemento(&nodo->izquierda, elemento);
+                printf("\nELEMENTO izq DE RAIZ: %d\n", *(int*)arbol->nodo_raiz->izquierda->elemento);  
+            }
         }
     }
     return insertado;
 }
 
 abb_t* arbol_crear(abb_comparador comparador, abb_liberar_elemento destructor) {
-    if (!comparador || !destructor) return NULL;
+    if (!comparador) return NULL;
 
     abb_t* arbol = (abb_t*)calloc(1, sizeof(abb_t));
     if (!arbol) return NULL;
--- pruebas.c
+++ pruebas.c
@@ -46,13 +46,13 @@ int comparador(void* elemento1, void* elemento2) {
 
     return resultado;
 }
-
+/*
 void pruebas_con_null() {
     abb_t* arbol_nulo = arbol_crear(NULL,NULL);
     pa2m_afirmar(arbol_nulo == NULL, "El arbol no se puede crear sin un comparador");
     arbol_destruir(arbol_nulo);
     arbol_nulo = arbol_crear(&comparador,NULL);
-    pa2m_afirmar(arbol_nulo == NULL, "El arbol no se puede crear sin un destructor");
+    pa2m_afirmar(arbol_nulo != NULL, "El arbol se puede crear sin un destructor");
     pa2m_afirmar(arbol_vacio(arbol_nulo) == true, "Un arbol nulo es considerado vacio");
     pa2m_afirmar(arbol_insertar(NULL, pesar_manzana(6)) == ERROR, "Devuelve ERROR al insertar un elemento en un arbol nulo");
     arbol_destruir(arbol_nulo);
@@ -61,9 +61,11 @@ void pruebas_con_null() {
     pa2m_afirmar(arbol_insertar(abb, NULL) == ERROR, "No se pudo insertar un elemento NULL");
     pa2m_afirmar(arbol_raiz(abb) == NULL, "El nodo raiz es NULL");
     arbol_destruir(abb);
+    //free(abb);
+
     //destruir
 
-}
+}*/
 
 void pruebas_de_abb_con_4_elementos() {
     //char elementos[] = {'a','c','b','d'};
@@ -77,17 +79,23 @@ void pruebas_de_abb_con_4_elementos() {
     pa2m_afirmar(arbol_vacio(abb) == false, "El abb no esta vacio");
     pa2m_afirmar(((manzana_t*)arbol_raiz(abb))->peso == 5, "La raiz del arbol es la esperada");
     pa2m_afirmar(arbol_insertar(abb, pesar_manzana(7)) == EXITO, "Se pudo insertar un elemento mayor");  
-    pa2m_afirmar(((manzana_t*)(abb->nodo_raiz->derecha->elemento))->peso == 7, "EL elemento derecho es el esperado");
+    pa2m_afirmar(((manzana_t*)(abb->nodo_raiz->derecha->elemento))->peso == 7, "EL elemento derecho de la raiz es el esperado");
     pa2m_afirmar(arbol_insertar(abb, pesar_manzana(3)) == EXITO, "Se pudo insertar un elemento menor");  
-    pa2m_afirmar(((manzana_t*)(abb->nodo_raiz->izquierda->elemento))->peso == 3, "EL elemento izquierdo es el esperado");
+    pa2m_afirmar(((manzana_t*)(abb->nodo_raiz->izquierda->elemento))->peso == 3, "EL elemento izquierdo de la raiz es el esperado");
     pa2m_afirmar(((manzana_t*)(arbol_buscar(abb,pesar_manzana(5))))->peso == 5, "Se encontro el 5");
     pa2m_afirmar(((manzana_t*)(arbol_buscar(abb,pesar_manzana(3))))->peso == 3, "Se encontro el 3");
+
     pa2m_afirmar(((manzana_t*)(arbol_buscar(abb,pesar_manzana(7))))->peso == 7, "Se encontro el 7");
     pa2m_afirmar(arbol_insertar(abb, pesar_manzana(1)) == EXITO, "Inserto el elemento 1");  
+    pa2m_afirmar(((manzana_t*)(abb->nodo_raiz->izquierda->izquierda->elemento))->peso == 1, "EL elemento izquierdo de la raiz es el esperado");
     pa2m_afirmar(arbol_insertar(abb, pesar_manzana(6)) == EXITO, "Inserto el elemento 6");  
     //pa2m_afirmar(arbol_insertar(abb, pesar_manzana(4)) == EXITO, "Inserto el elemento 4"); 
     //pa2m_afirmar(arbol_insertar(abb, pesar_manzana(5)) == EXITO, "Inserto el elemento 5");  
     pa2m_afirmar(arbol_insertar(abb, pesar_manzana(8)) == EXITO, "Inserto el elemento 8");
+    //pa2m_afirmar(abb->nodo_raiz->derecha->derecha->derecha, "8 tiene hijo");
+
+    
+
    // pa2m_afirmar(arbol_insertar(abb, pesar_manzana(7)) == EXITO, "Inserto el elemento 7");  
     //pa2m_afirmar(arbol_insertar(abb, pesar_manzana(8)) == EXITO, "Inserto el elemento 8");
     //pa2m_afirmar(arbol_insertar(abb, pesar_manzana(7)) == EXITO, "Inserto el elemento 7");
@@ -95,18 +103,42 @@ void pruebas_de_abb_con_4_elementos() {
     pa2m_afirmar(((manzana_t*)(arbol_buscar(abb,pesar_manzana(5))))->peso == 5, "Se encontro el 5");
     pa2m_afirmar(((manzana_t*)(arbol_buscar(abb,pesar_manzana(7))))->peso == 7, "Se encontro el 7");
     pa2m_afirmar(((manzana_t*)(arbol_buscar(abb,pesar_manzana(8))))->peso == 8, "Se encontro el 8");
+    pa2m_afirmar(arbol_buscar(abb,pesar_manzana(10)) == (void*)NULL, "NO se encontro el 10");
+    pa2m_afirmar(arbol_buscar(abb,pesar_manzana(11)) == (void*) NULL, "NO se encontro el 11");
     //pa2m_afirmar(((manzana_t*)(arbol_buscar(abb,pesar_manzana(4))))->peso == 4, "Se encontro el 4");
-    pa2m_afirmar(arbol_buscar(abb,pesar_manzana(10)) == (void*)NULL, "no Se encontro el 10");
-    pa2m_afirmar(arbol_buscar(abb,pesar_manzana(11)) == (void*) NULL, "no Se encontro el 11");
-    //pa2m_afirmar(((manzana_t*)(arbol_buscar(abb,pesar_manzana(8))))->peso == 7, "Se encontro el 8");
+    
+
     pa2m_afirmar(arbol_borrar(abb, pesar_manzana(5)) == EXITO, "Se pudo borrar el elemento 5");
     pa2m_afirmar(arbol_buscar(abb,pesar_manzana(5)) == (void*) NULL, "No se encontro el 5");
+    pa2m_afirmar(((manzana_t*)arbol_raiz(abb))->peso == 6, "La raiz del arbol es la esperada");
+    
+    pa2m_afirmar(((manzana_t*)(abb->nodo_raiz->derecha->elemento))->peso == 7, "EL elemento derecho de la raiz es el esperado");
+    pa2m_afirmar(((manzana_t*)(abb->nodo_raiz->izquierda->elemento))->peso == 3, "EL elemento izquierdo de la raiz es el esperado");
+   
+
     pa2m_afirmar(((manzana_t*)(arbol_buscar(abb,pesar_manzana(1))))->peso == 1, "Se encontro el 1");
-    //pa2m_afirmar(arbol_borrar(abb, pesar_manzana(1)) == EXITO, "Se pudo borrar el elemento 1");
-    //pa2m_afirmar(arbol_buscar(abb,pesar_manzana(1)) == (void*) NULL, "No se encontro el 1");
-    //pa2m_afirmar(arbol_destruir(abb,pesar_manzana(1)) == (void*) NULL, "No se encontro el 1");
+    pa2m_afirmar(arbol_borrar(abb, pesar_manzana(1)) == EXITO, "Se pudo borrar el elemento 1");
+    pa2m_afirmar(arbol_buscar(abb,pesar_manzana(1)) == (void*) NULL, "No se encontro el 1");
+   // pa2m_afirmar(arbol_buscar(abb,pesar_manzana(1)) != 1, "No se encontro el 1");
+    pa2m_afirmar(arbol_borrar(abb, pesar_manzana(7)) == EXITO, "Se pudo borrar el elemento 7");
+    pa2m_afirmar(((manzana_t*)(abb->nodo_raiz->derecha->elemento))->peso == 8, "EL elemento derecho de la raiz es el esperado");
+
+    pa2m_afirmar(arbol_borrar(abb, pesar_manzana(6)) == EXITO, "Se pudo borrar el elemento 6");
+    
+
+    pa2m_afirmar(arbol_borrar(abb, pesar_manzana(3)) == EXITO, "Se pudo borrar el elemento 3");
+   
+
+    pa2m_afirmar(arbol_borrar(abb, pesar_manzana(8)) == EXITO, "Se pudo borrar el elemento 8");
+    pa2m_afirmar(arbol_vacio(abb) == true, "El arbol esta vacio");
+    
+
+   // pa2m_afirmar(arbol_borrar(abb, pesar_manzana(1)) == ERROR, "No se pudo borrar el elemento 1");
+    
+    
     arbol_destruir(abb);
-    pa2m_afirmar(arbol_vacio(abb) == true, "Se pudo destruir el arbol");
+    //if (!abb) printf("Se pudo destruir el arbol");
+    
     
     //destruir
     
@@ -128,7 +160,7 @@ int main() {
     pruebas_de_abb_con_4_elementos();
 
     pa2m_nuevo_grupo("PRUEBAS CON NULL");
-    pruebas_con_null();
+   // pruebas_con_null();
   /*  
     pa2m_nuevo_grupo("PRUEBAS DE ABB CON UN UNICO ELEMENTO");
     pruebas_de_abb_unitaro();
